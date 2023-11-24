9b61ec7dbf6d9bafb402bb09b504d8a4d6ef217c
Nicolas Avila <nicolas.avila09@outlook.com>
2023-11-23 21:03:59 -0300

    primer subida con react

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Dolce Fiore </h1>
+      <h2>Pasteleria y Chocolateria</h2>
+    </>
+  );
+}
+
+export default App;
